c555599cd8d83047562f966f7d16dc04e56f4fa7
Mousuf C Zaman <mousus.nayon@gmail.com>
2026-05-12 19:10:27 +0100

    Bio added, python script added.

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -22,8 +22,18 @@ export const aboutMe: AboutMe = {
   title: "Software Developer",
   institution: "London, UK",
   // Note that links work in the description
-  description:
-    "",
+  description: `
+    <p>
+      <strong>Adventure seeker, Software Developer,</strong> and a man passionate about science, maths, art, and literature. My current passion project focuses on material science simulation software and the study of Automata Theory and Formal Language Theory.<br><br>
+    </p>
+    <p>
+      I frequently post <a href="https://substack.com/@mousufnayon" target="_blank" rel="noopener noreferrer">analyse articles</a> of academic papers, industry articles and reports. However, one thing you won’t see me post about but I thoroughly enjoy is the political analysis of speeches and books. <br><br>
+    </p>
+    <ul>
+      <li><strong>Currently Reading:</strong> <em>Dune</em> and the analysis of the characters' political influence on the storyline.</li>
+      <li><strong>Speech Analysis:</strong> Alexander the Great & The Opis Mutiny. Linguistic analysis and the historical impact of the speech. </li>
+    </ul>
+  `,
   email: "mousuf.nayon@gmail.com",
   imageUrl:
     "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
